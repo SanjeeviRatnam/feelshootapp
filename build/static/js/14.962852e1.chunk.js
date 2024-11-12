b75c00202c01aa4e036455d1ee9ea3a4badca4d0
SanjeeviRatnam <kasasandhya1@gmail.com>
2024-11-12 03:04:11 -0800

    Build folder

---
+++ build/static/js/14.962852e1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-tailwindcss-portfolio"]=this["webpackJsonpreact-tailwindcss-portfolio"]||[]).push([[14],{57:function(t,e,a){"use strict";a.r(e);var i=a(3);e.default=()=>Object(i.jsx)("div",{className:"container mx-auto",children:Object(i.jsx)("div",{className:"text-center text-ternary-dark dark:text-primary-light",children:"Marketing Page"})})}}]);
+//# sourceMappingURL=14.962852e1.chunk.js.map
